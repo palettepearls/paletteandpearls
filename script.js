fd4c31ffcff3bdd33a1884441ceb4d41659eb09d
palettepearls <chatterjeep0795@yahoo.com>
2026-02-22 21:19:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,103 +1,54 @@
 let products = JSON.parse(localStorage.getItem("products")) || [
-
-{
-name:"Handmade Earrings",
-price:199,
-image:"product1.jpg"
-},
-
-{
-name:"Clay Plate",
-price:499,
-image:"product2.jpg"
-},
-
-{
-name:"Fridge Magnet",
-price:149,
-image:"product3.jpg"
-}
-
+{name:"Earrings",price:199,image:"https://via.placeholder.com/300"},
+{name:"Clay Plate",price:499,image:"https://via.placeholder.com/300"},
+{name:"Fridge Magnet",price:149,image:"https://via.placeholder.com/300"}
 ];
 
-let cart = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function loadProducts()
-{
+function saveCart(){
+localStorage.setItem("cart",JSON.stringify(cart));
+}
 
+function loadProducts(){
 let html="";
-
-products.forEach((p,i)=>
-{
-
+products.forEach((p,i)=>{
 html+=`
-
 <div class="product">
-
 <img src="${p.image}">
-
 <h3>${p.name}</h3>
-
-<div class="price">₹${p.price}</div>
-
-<button onclick="addToCart(${i})">
-Add to Cart
-</button>
-
-</div>
-
-`;
-
+<p>₹${p.price}</p>
+<button onclick="addToCart(${i})">Add to Cart</button>
+</div>`;
 });
-
 document.getElementById("productGrid").innerHTML=html;
-
 }
 
-function addToCart(i)
-{
-
+function addToCart(i){
 cart.push(products[i]);
-
+saveCart();
 loadCart();
-
 }
 
-function loadCart()
-{
-
+function loadCart(){
 let html="";
 let total=0;
-
-cart.forEach(p=>
-{
-
-html+=`${p.name} - ₹${p.price}<br>`;
-
+cart.forEach(p=>{
+html+=`${p.name} ₹${p.price}<br>`;
 total+=Number(p.price);
-
 });
-
 document.getElementById("cartItems").innerHTML=html;
-
 document.getElementById("total").innerHTML="Total: ₹"+total;
-
+document.getElementById("cartCount").innerHTML=cart.length;
 }
 
-function checkout()
-{
-
+function checkout(){
 let msg="Hello, I want to order:%0A";
-
-cart.forEach(p=>
-{
-
+cart.forEach(p=>{
 msg+=p.name+" ₹"+p.price+"%0A";
-
 });
-
 window.open("https://wa.me/918670322260?text="+msg);
-
 }
 
 loadProducts();
+loadCart();
